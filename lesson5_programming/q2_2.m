2d4cd2f27225a3c355789c8a29d0d713a55038f7
Gerard Capes <gerard.capes@manchester.ac.uk>
2017-10-24 10:18:25 +0100

    Correct solutions to variable type questions

--- lesson5_programming/q2_2.m
+++ lesson5_programming/q2_2.m
@@ -1,24 +1,24 @@
 % 2.2 (a)
 
-if class(A)=='double'
+if isa(A,'double')
     disp('A is double precision')
 end
 
 % 2.2 (b)
-if class(A)=='double'
+if isa(A,'double')
     disp('A is double precision')
-elseif class(A)=='char'
+elseif isa(A,'char')
     disp('A is a character')
-elseif class(A)=='logical'
+elseif isa(A,'logical')
     disp('A is a logical')
 end
 
 % 2.2 (c)
-if class(A)=='double'
+if isa(class(A),'double')
     disp('A is double precision')
-elseif class(A)=='char'
+elseif isa(A,'char')
     disp('A is a character')
-elseif class(A)=='logical'
+elseif isa(A,'logical')
     disp('A is a logical')
 else
     disp('Unknown class')
